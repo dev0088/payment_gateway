5a575fce17beb1e04a96361df0caa1c67afbd22e
ninjadev999 <ninjadev999@gmail.com>
2019-11-01 10:25:49 -0700

    added frontend build directory

---
+++ frontend/build/js/stylesheets.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["stylesheets"],{
+
+/***/ "./node_modules/perfect-scrollbar/css/perfect-scrollbar.css":
+/*!******************************************************************!*\
+  !*** ./node_modules/perfect-scrollbar/css/perfect-scrollbar.css ***!
+  \******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./node_modules/perfect-scrollbar/css/perfect-scrollbar.css?");
+
+/***/ })
+
+}]);
